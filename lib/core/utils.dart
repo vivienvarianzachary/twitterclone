c7b333247f364eb18b0c05d6912527812335b4f8
Briliantoro <gakyasa@gmail.com>
2023-07-20 22:53:38 +0700

    picking multiple images dan displaying in carousel

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,10 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 void showSnackbar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text(message),
-  ));
-}
-String getNameFromEmail(String email) {
+@@ -10,4 +13,16 @@ String getNameFrom(String email) {
+  //alfiooatmaja@gmail.com
+  //List = [alfiooatmaja@gmail.com]
   return email.split('@')[0];
 }
 
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final imageFiles = await picker.pickMultiImage();
+  if (imageFiles.isNotEmpty) {
+    for (final image in imageFiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
+}
